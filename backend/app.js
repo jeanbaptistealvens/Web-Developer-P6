0ee19034ebe7071cdcace7ccd71d2202541d817d
Jean Baptiste Alvens <113525179+jeanbaptistealvens@users.noreply.github.com>
2023-02-11 01:08:03 +0100

    back terminé

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const saucesRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
+const path = require('path');
+
+mongoose.connect('mongodb+srv://JEAN-BAPTISTE-Alvens:s46frGbw2S9Taa@atlascluster.qg56vpj.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/api/sauces', saucesRoutes)
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+module.exports = app;
